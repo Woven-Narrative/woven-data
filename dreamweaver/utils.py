0b5e34a96b68b6188bdebd24f576ddb731dde64f
thomaslilo <thomas.j.lilo@gmail.com>
2023-05-31 08:52:31 -0400

    docstrings

--- dreamweaver/utils.py
+++ dreamweaver/utils.py
@@ -12,10 +12,12 @@ def embed_request(query):
     return embedding
 
 def eval_individual_texts(query, text_list):
-    # Please summarize the response. 
-    # Also, which elements of the response are those which were asked for
-    # in the query? Highlight them.
-    # Return a string of text with the response.
+    """
+    Please summarize the response. 
+    Also, which elements of the response are those which were asked for
+    in the query? Highlight them.
+    Return a string of text with the response.
+    """
     prompt_intro = """Here are some criteria and an excerpt about a mining project. Can you please
                       summarize the key points about the mining project mentioned in 
                       the excerpt and tell me how many of
@@ -36,9 +38,11 @@ def eval_individual_texts(query, text_list):
     return r_list
 
 def eval_list_texts(query, responses, n = 5):
-    # Given a list of project summaries, return the n most relevant
-    # The most relevant should be decided by how much they fit the criteria
-    # Return a list of the most relevant texts, and why they're relevant
+    """
+    Given a list of project summaries, return the n most relevant
+    The most relevant should be decided by how much they fit the criteria
+    Return a list of the most relevant texts, and why they're relevant
+    """
     delimiter = ' | '
     split_list = delimiter.join(responses)
     final_eval_prompt = 'Please read through the following mining project summaries and recommend the top 5 based on the number of criteria that they meet. '
@@ -64,7 +68,13 @@ def make_query(query):
 # cache to prevent multiple calls to the API
 @st.cache
 def match_documents(match_threshold: float, match_count: int, query_embedding: str, baseurl: str, token: str): 
-        
+    """
+    Returns a set of matched DB rows based on the files cosine similarity.
+    Match threshold is the minimum match score you want in your responses.
+    Match count is the max number of rows you want returned.
+    base url is the supabase url, token is the secret token.
+    query embeddings is the openai embedding that you want to compare to the docs.
+    """
     # prepare the elements of the request
     headers = {
             'Content-Type': 'application/json',
@@ -90,8 +100,14 @@ def match_documents(match_threshold: float, match_count: int, query_embedding: s
     else:
         return response.status_code
     
-def query_supabase(query: str, baseurl: str, token: str, return_count: int):
+def supabase_match_documents(query: str, baseurl: str, token: str, return_count: int):
+    """
+    Use the supabase match_documents endpoint to request similar documents then check them with chat gpt before returning.
+    """
     user_embedding = embed_request(query)
     # just set it as threshold 0.78 to see what happens
     result = match_documents(match_threshold=0.78, match_count=return_count, query_embedding=user_embedding, baseurl=baseurl, token=token)
+    #
+    # add additional steps here !!
+    #
     return result
