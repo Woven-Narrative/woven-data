b4a08e18d47bf6672ff4cf55c8b2b3b7430f3708
thomaslilo <thomas.j.lilo@gmail.com>
2023-06-02 01:58:28 -0400

    user interface

--- app.py
+++ app.py
@@ -3,28 +3,104 @@ To Run:
 streamlit run SearchDemo.py
 """
 import streamlit as st
-import openai
-import dreamweaver.Chains as Chains
-
+from datetime import date
+import pandas as pd
 
 # Defining The Application UI
 def main():    
     
-    st.title("Woven Mining Project Search")    
+    st.set_page_config(
+        page_title="Woven Mining Project Search",
+        page_icon=":pick:",
+        layout="wide",
+    )
+    
+    df = pd.DataFrame(columns=['Project', 'Company', 'Date', 'LLM Response'])
+    
+    # these are stored and used in queries
+    filters = {
+        "resource_type":[],
+        "geography": [],
+        "company":[],
+        "start_date":date(2017, 7, 6),
+        "end_date":date(2022, 7, 6),
+        "development_stage":"",
+        "is_operating":True
+    }
+    
+    request = {}
+    
+    # Sidebar ===============================
+    
+    with st.sidebar:        
+        
+        st.markdown("# **Project Filters:**")
+        
+        filters["is_operating"] = st.checkbox("Site Is Operating", value=True)
+        
+        filters["resource_type"] = st.multiselect(
+            'Resource Type(s)',
+            ['Gold', 'Copper', 'Lithuim', 'Coal',"Urnanium",
+             "Cobalt","Zinc","Nickle","Iron","Silver",
+             "Aluminum","Tin","Palladium","Chromite",
+             "Vanadium","Titanium","Zirconium"
+             ]
+        )
+        
+        filters["company"] = st.multiselect(
+            'Companies(s)',
+            ['Agnico Eagle','Alamos Gold','Kinross Gold','Teck Resources','Barrick','Glencore',
+             'Rio Tinto','Anglo American','Yamana Gold','Silvercorp Metals','SSR Mining','Newmont',
+             'Hudbay Minerals','B2Gold Corp','American Lithium']
+        )
+        
+        filters["start_date"] = st.date_input(
+                                "Start Date",
+                                date(2017, 7, 6))
+                                
+        filters["start_date"] = st.date_input(
+                                "End Date",
+                                date(2020, 7, 6))                    
 
+        filters["geography"] = st.multiselect(
+            'Geographies(s)',
+            ['Canada','United States','Mexico','Congo','South Africa','Germany',
+             'Columbia','Chile','Peru','France','United Kingdom','Australia',
+             'Bolivia','Angola','Sierra Leon']
+        )
+
+    # Main Area ===============================
+    
+    st.title("Woven Mining Project Search") 
+        
     user_input = st.text_area(
-        "Describe the criteria of your project", 
+        "Describe The Criteria of your Project:", 
         value="", 
-        height=300, 
+        height=100, 
         key="user_input"
+    )
+
+    st.markdown("""        
+        Examples:
+        
+        *Copper projects that have their own power supplies.*
+        
+        *Gold mines that have produced over 10 MT.* 
+    """
     )
 
     if st.button('Search'):
         # Here you can call your function using the user's input
-        #response = utils.make_query(user_input)
-        response = Chains.query_supabase(query=user_input, baseurl=baseurl, token=token, return_count=15)
-        # Write the result to the page
-        st.write(response)
+        #response = Chains.query_supabase(query=user_input, return_count=15)
+        request = filters
+
+
+    
+    st.dataframe(df, use_container_width=True)
+    
+    st.json(request)
+    
+    
 
 # run the app
 if "__main__" == __name__:
