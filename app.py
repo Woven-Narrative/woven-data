f67afaefe260312463ef793d6674b4e3c6087ceb
Thomas Lillo <thomas.j.lillo@gmail.com>
2024-03-30 03:06:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 import streamlit as st
 from datetime import date
 import pandas as pd
-from dreamweaver.chains import Chains
+from database import Database
+from annotated_text import annotated_text
 
 # Defining The Application UI
-def main():    
-    
+def main():        
+
     # needs to be called first
     st.set_page_config(
-        page_title="Woven Mining Project Search",
+        page_title="Woven Mining",
         page_icon=":pick:",
         layout="wide",
     )
-    
-    # setup chains
-    @st.cache
-    def setup_chains():        
-        chains = Chains()
-        return chains
-    
-    chains = setup_chains()
-    
+
+    url = st.secrets["XATA_DATABASE_URL"]
+    key = st.secrets["XATA_API_KEY"]
+   
     # setup filters
+
+    db = Database(url, key)
     
     df = pd.DataFrame(columns=['Project', 'Company', 'Date', 'LLM Response'])
     
@@ -31,14 +29,11 @@ def main():
         "geography": [],
         "company":[],
         "start_date":date(2017, 7, 6),
-        "end_date":date(2022, 7, 6),
+        "end_date":date(2023, 7, 6),
         "development_stage":"",
         "is_operating":True,
         "return_max":50
-    }
-    
-    # temp var to show us what the request looks like in development
-    request = {}
+    }    
     
     # Sidebar ===============================
     
@@ -46,11 +41,13 @@ def main():
     # how does the company set their reserve price vs market price
     # have market prices for resources selected on the screen    
     
-    with st.sidebar:        
+    with st.sidebar:                
+
+        st.title("A Sample of Woven Mining's Technical Reports Dataset")
+
+        st.markdown("## **Filters (Not Implemented):**")
         
-        st.markdown("# **Project Filters:**")
-        
-        filters["is_operating"] = st.checkbox("Site Is Operating", value=True)
+        #filters["is_operating"] = st.checkbox("Site Is Operating", value=True)
         
         filters["resource_type"] = st.multiselect(
             'Resource Type(s)',
@@ -63,7 +60,7 @@ def main():
         
         filters["company"] = st.multiselect(
             'Companies(s)',
-            ['Agnico Eagle','Alamos Gold','Kinross Gold','Teck Resources','Barrick','Glencore',
+            ['Tribeca Resources','Agnico Eagle','Alamos Gold','Kinross Gold','Teck Resources','Barrick','Glencore',
              'Rio Tinto','Anglo American','Yamana Gold','Silvercorp Metals','SSR Mining','Newmont',
              'Hudbay Minerals','B2Gold Corp','American Lithium']
         )
@@ -72,48 +69,40 @@ def main():
                                 "Start Date",
                                 date(2017, 7, 6))
                                 
-        filters["start_date"] = st.date_input(
+        filters["end_date"] = st.date_input(
                                 "End Date",
                                 date(2020, 7, 6))                    
 
         filters["geography"] = st.multiselect(
             'Geographies(s)',
-            ['Canada','United States','Mexico','Congo','South Africa','Germany',
-             'Columbia','Chile','Peru','France','United Kingdom','Australia',
+            ['Canada','Australia','United States','Chile','Mexico','Peru','Congo','South Africa','Germany','China',
+             'France','United Kingdom','Indonesia'
              'Bolivia','Angola','Sierra Leon']
         )
         
-        filters["return_max"] = st.number_input("Max Response Length:", 
-                                                step=1, 
-                                                max_value=100, 
-                                                min_value=1
-                                            )
+        #filters["return_max"] = st.number_input("Max Response Length:", step=1, max_value=100, min_value=1)
 
     # Main Area ===============================
     
-    st.title("Woven Mining Project Search") 
+    st.title("NI 43-101 Explorer") 
         
     user_input = st.text_area(
-        "Describe The Criteria of your Project:", 
+        "Enter Search Text", 
         value="", 
-        height=100, 
+        height=1, 
         key="user_input"
-    )
-
-    st.markdown("""        
-        Examples:
-        
-        *Copper projects that have their own power supplies.*
-        
-        *Gold mines that have produced over 10 MT.* 
-    """
     )
 
     if st.button('Search'):
         # Here you can call your function using the user's input
-        response = chains.find_similar_projects(criteria=user_input, filters=filters)    
-        #st.dataframe(df, use_container_width=True)        
-        st.write(response)
+        response = db.search_summarys(searchtext=user_input)    
+        #st.dataframe(df, use_container_width=True)
+        for file in response:
+            st.markdown(f"#### {file}")
+            v = response[file]
+            annotated_text(v)
+
+        #st.write(response)
 
 # run the app
 if "__main__" == __name__:
